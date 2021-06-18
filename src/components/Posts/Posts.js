cd0d06230105c803cd73a3b24237ec56c1b8d6be
stkirk <shaanekirk@gmail.com>
2021-06-18 17:48:05 -0400

    in posts component mapped over posts data and returned individual post components with post data, unique keys, and the likePost helper function passed down to Post as props

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -5,11 +5,18 @@ import './Posts.css';
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
+  console.log("Posts props", props);
+  
 
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
+      {
+        posts.map(post => {
+          return <Post post={post} key={post.id} likePost={likePost} />
+        })
+      }
     </div>
   );
 };
